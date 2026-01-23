48ae060e8282060b09161d4226a3c9c00b58e84d
haneulworks <155050258+haneulworks@users.noreply.github.com>
2026-01-23 09:23:02 +0900

    줄바꿈 처리 로직 추가

--- resource/js/common.js
+++ resource/js/common.js
@@ -58,7 +58,7 @@ function renderNotice(notice) {
 
   notice.forEach(text => {
     const li = document.createElement('li');
-    li.textContent = text;
+    li.innerHTML = text.replace(/\n/g, '<br>');
     ul.appendChild(li);
   });
 }
